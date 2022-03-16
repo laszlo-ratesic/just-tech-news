64a3a9195e439e8b7a2d98d3e2fa9993ba0adc10
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-03-16 17:02:15 -0500

    refactor vote routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -7,7 +7,18 @@ router.get("/", (req, res) => {
   console.log("================");
   Post.findAll({
     // Query configuration
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
     order: [["created_at", "DESC"]],
     include: [
       {
@@ -28,7 +39,18 @@ router.get("/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
     include: [
       {
         model: User,
@@ -66,35 +88,12 @@ router.post("/", (req, res) => {
 
 // PUT /api/posts/upvote
 router.put("/upvote", (req, res) => {
-  Vote.create({
-    user_id: req.body.user_id,
-    post_id: req.body.post_id
-  })
-    .then(() => {
-      // find the post we just voted on
-      return Post.findOne({
-        where: {
-          id: req.body.post_id,
-        },
-        attributes: [
-          "id",
-          "post_url",
-          "title",
-          "created_at",
-          [
-            // use raw MySQL aggregate function query to get a count of how many votes a post has and return it under the name 'vote_count'
-            sequelize.literal(
-              "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-            ),
-            "vote_count"
-          ]
-        ]
-      })
-        .then((dbPostData) => res.json(dbPostData))
-        .catch((err) => {
-          console.log(err);
-          res.status(400).json(err);
-        });
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+    .then((updatePostData) => res.json(updatePostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
     });
 });
 
