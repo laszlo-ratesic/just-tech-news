435b1a4dfa054808ddf1e04b0e90ae62aa364e8f
laszlo-ratesic <keenanrc@hotmail.com>
2022-03-21 11:35:04 -0500

    add helpers

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,5 +1,6 @@
 const {format_date} = require('../utils/helpers');
 const {format_plural} = require('../utils/helpers');
+const {format_url} = require('../utils/helpers');
 
 test('format_date() returns a date string', () => {
     const date = new Date('2020-03-21 10:29:03');
@@ -17,4 +18,14 @@ test('format_plural() returns a plural string', () => {
     amount = 1
 
     expect(format_plural(obj, amount)).toBe("lion")
+})
+
+test('format_url() returns a simplified url string', () => {
+    const url1 = format_url('http://test.com/page/1');
+    const url2 = format_url('https://www.coolstuff.com/abcdefg');
+    const url3 = format_url('https://www.google.com?q=hello');
+
+    expect(url1).toBe('test.com');
+    expect(url2).toBe('coolstuff.com');
+    expect(url3).toBe('google.com');
 })
